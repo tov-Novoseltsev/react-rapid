c6080fe288f19b28a4027d377117171bb69f0f20
Maxim Novoseltsev <mnovoseltsev@accesssoftek.com>
2015-11-16 09:24:26 +0200

    Added another handy data utility method

--- src/Data/DataUtility.js
+++ src/Data/DataUtility.js
@@ -9,3 +9,7 @@ module.exports.getListItemFromValue = function(val) {
 module.exports.getFirstValue = function(array) {
   return _.first(_.map(array, function(item) { return item.value; }));
 };
+
+module.exports.getItemNameByValue = function(array, val) {
+  return _.result(_.find(array, { 'value': val }), 'name');
+};
